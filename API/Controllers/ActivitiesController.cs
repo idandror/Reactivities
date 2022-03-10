3b8b2be2c65b18cc79e1905f7446e005592c9441
idandror <eidan.dror@devalore.com>
2022-03-10 14:29:00 +0200

    added edit and delete routes

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -22,9 +22,24 @@ namespace API.Controllers
         public async Task<ActionResult<Activity>> GetActivity(Guid id){
             return await Mediator.Send(new Details.Query{Id=id});
         }
-
+        [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity){
             return Ok(await Mediator.Send(new Create.Command {Activity=activity}));
         }
-	}       
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EdiActivity(Guid id,Activity activity)
+        {
+                activity.Id=id;
+                return Ok(await Mediator.Send(new Edit.Command{Activity=activity}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id=id}));
+        }
+
+	}      
+
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Application.Activities;
 using MediatR;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -15,6 +16,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistance;
+using Application.Core;
+using API.Extensions;
 
 namespace API
 {
@@ -33,19 +36,7 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-           services.AddDbContext<DataContext>(opt=>{
-               opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-           });
-           services.AddCors(opt=>{
-               opt.AddPolicy("CorsPolicy",policy=>{
-                   policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-               });
-           });
-           services.AddMediatR(typeof(List.Handler).Assembly);
+           services.AddApplicationServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
