bf1732ee115c3d33293d884480f1d33363b0b9fb
idandror <eidan.dror@devalore.com>
2022-03-13 16:53:48 +0200

    starting authentication and validation of the users in the backend

--- Persistance/DataContext.cs
+++ Persistance/DataContext.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Persistance
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options):base(options)
         {
